4d983244a15a4cbb06588ea7ac692540c7270170
Calvin Yoh <flashfire340@gmail.com>
2020-05-17 16:05:19 -0700

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,42 +1,84 @@
-/* Author: cyoh001
-* Partner(s) Name :
-	*Lab Section :
-*Assignment : Lab #  Exercise #
-* Exercise Description : [optional - include for your own benefit]
-*
-*I acknowledge all content contained herein, excluding template or example
-* code, is my own original work.
-*/
+/*	Author:
+ *  	Partner(s) Name:
+ *   	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+
+
+
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include "io.c"
+#include "io.h"
 
-enum States { Start, Up, Down, Begin, Wait, OnOff } state;
-double array[8] = { 261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25 };
-double temp = 0;
-unsigned char min = 0x00;
-unsigned char max = 0x07;
-unsigned char current = 0x00;
-unsigned char currentMode = 0x00;
-unsigned char offHolder = 0x00;
-unsigned char tempA = 0x00;
-
-void set_PWM(double frequency)
+volatile unsigned char TimerFlag = 0;
+unsigned long _avr_timer_M = 1; // Start count from here, down to 0. Default 1 ms.
+unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1 ms ticks
+s
+void TimerOn()
 {
-	static double current_frequency;
-	if (frequency != current_frequency)
+
+	TCCR1B = 0x0B;
+	OCR1A = 125;
+	TIMSK1 = 0x02;
+	TCNT1 = 0;
+	_avr_timer_cntcurr = _avr_timer_M;
+	SREG |= 0x80;
+}
+void TimerOff()
+{
+	// bit3bit1bit0=000: timer off
+	TCCR1B = 0x00;
+}
+void TimerISR()
+{
+	TimerFlag = 1;
+}
+ISR(TIMER1_COMPA_vect)
+{
+	_avr_timer_cntcurr--;
+	if (_avr_timer_cntcurr == 0)
 	{
-		if (!frequency) TCCR3B &= 0x08;
-		else TCCR3B |= 0x03;
+		TimerISR();
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+void TimerSet(unsigned long M)
+{
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
+
+
+
+
+
+
+void set_PWM(double frequency) {
+	static double current_frequency;
+	if (frequency != current_frequency) {
+
+		if (!frequency) TCCR3B &= 0x08; //stops timer/counter
+		else TCCR3B |= 0x03; // resumes/continues timer/counter
 		if (frequency < 0.954) OCR3A = 0xFFFF;
 		else if (frequency > 31250) OCR3A = 0x0000;
 		else OCR3A = (short)(8000000 / (128 * frequency)) - 1;
-		TCNT3 = 0;
+		TCNT3 = 0; // resets counter
 		current_frequency = frequency;
 	}
 }
 void PWM_on() {
 	TCCR3A = (1 << COM3A0);
+	// COM3A0: Toggle PB6 on compare match between counter and OCR3A
 	TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+	// WGM32: When counter (TCNT3) matches OCR3A, reset counter
+	// CS31 & CS30: Set a prescaler of 64
 	set_PWM(0);
 }
 void PWM_off() {
@@ -44,126 +86,71 @@ void PWM_off() {
 	TCCR3B = 0x00;
 }
 
+double tone[9] = { 2610.63, 2930.66, 3290.63, 3490.23, 3920.00, 4400.00, 4930.88, 5230.25,0 };
+unsigned char counter = 9;
+unsigned char onFlag = 0;
+enum music { Start, INIT, PLAY, WAIT } state;
+
 void Tick()
 {
-	tempA = ~PINA & 0x07;
-	switch (state) // transitions
+	switch (state)
 	{
 	case Start:
 	{
-		state = Begin;
+		state = INIT;
 		break;
 	}
-	case Begin:
+	case INIT:
 	{
-		if (tempA == 0x01)
+		if ((~PINA & 0x01) == 0x01)
 		{
-			state = Up;
-			break;
-		}
-		else if (tempA == 0x02)
-		{
-			state = Down;
-			break;
-		}
-		else if (tempA == 0x04)
-		{
-			state = OnOff;
+			state = PLAY;
 			break;
 		}
 		else
 		{
-			state = Begin;
+			state = INIT;
 			break;
 		}
 	}
-	case Up:
+	case PLAY:
 	{
-		temp = array[current];
-		state = Wait;
 		break;
 	}
-	case Down:
+	case WAIT:
 	{
-		temp = array[current];
-		state = Wait;
-		break;
-	}
-	case Wait:
-	{
-		if (tempA == 0x00)
-		{
-			state = Begin;
-			break;
-		}
-		else
-		{
-			state = Wait;
-			break;
-		}
-	}
-	case OnOff:
-	{
-		state = Wait;
-		break;
+		state = PLAY;
 	}
 	default:
 		break;
 	}
-	switch (state) // state actions
+	switch (state)
 	{
-	case Up:
+	case Start:
+		break;
+	case INIT:
 	{
-		if ((current + 1) > max)
-		{
-			current = max;
-			break;
-		}
-		else
-		{
-			current++;
-			break;
-		}
-	}
-	case Down:
-	{
-		if ((current - 1) < min)
-		{
-			current = min;
-			break;
-		}
-		else
-		{
-			current--;
-			break;
-		}
-	}
-	case Wait:
-	{
-		temp = array[current];
-		set_PWM(temp);
+		set_PWM(0);
 		break;
 	}
-	case OnOff:
+	case PLAY:
 	{
-		if (currentMode == 0x01)
+		if (counter > 0)
 		{
-			PWM_off();
-			offHolder = current;
-			set_PWM(0);
-			currentMode = 0;
+			set_PWM(tone[counter]);
+			counter--;
+			state = WAIT;
 			break;
 		}
-		else
-		{
-			PWM_on();
-			current = offHolder;
-			currentMode = 1;
-			break;
-		}
+
+	}
+	case WAIT:
+	{
+		break;
 	}
 	default:
 		break;
+
 	}
 }
 
@@ -173,10 +160,14 @@ int main(void)
 	DDRB = 0xFF; PORTB = 0x00;
 	state = Start;
 	PWM_on();
-	currentMode = 1;
-
+	TimerSet(50);
+	TimerOn();
 	while (1)
 	{
+
 		Tick();
+		while (!TimerFlag);
+		TimerFlag = 0;
+
 	}
 }
