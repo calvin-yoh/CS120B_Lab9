b9f8e0e1d5b1dbe8761fd0d8ed4641f88659195c
Calvin Yoh <flashfire340@gmail.com>
2020-05-17 17:33:14 -0700

    update

--- source/main.c
+++ source/main.c
@@ -8,19 +8,15 @@
  *	code, is my own original work.
  */
 
-
-
-
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include "io.h"
 
 volatile unsigned char TimerFlag = 0;
-unsigned long _avr_timer_M = 1; // Start count from here, down to 0. Default 1 ms.
-unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1 ms ticks
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+
 void TimerOn()
 {
-
 	TCCR1B = 0x0B;
 	OCR1A = 125;
 	TIMSK1 = 0x02;
@@ -54,17 +50,17 @@ void TimerSet(unsigned long M)
 void set_PWM(double frequency) {
 	static double current_frequency;
 	if (frequency != current_frequency) {
-		if (!frequency) TCCR3B &= 0x08; //stops timer/counter
-		else TCCR3B |= 0x03; // resumes/continues timer/counter
+		if (!frequency) TCCR3B &= 0x08;
+		else TCCR3B |= 0x03;
 		if (frequency < 0.954) OCR3A = 0xFFFF;
 		else if (frequency > 31250) OCR3A = 0x0000;
 		else OCR3A = (short)(8000000 / (128 * frequency)) - 1;
-		TCNT3 = 0; // resets counter
+		TCNT3 = 0;
 		current_frequency = frequency;
 	}
 }
 void PWM_on() {
-	TCCR0A = (1 << COM0A0 | 1 << WGM00);
+	TCCR3A = (1 << COM3A0);
 	TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
 	set_PWM(0);
 }
@@ -73,60 +69,133 @@ void PWM_off() {
 	TCCR3B = 0x00;
 }
 
+enum States { Start, WAIT, PLAY, DOWN, PAUSE, WAIT_2 }state;
+double notes[22] = { 830.61, 622.25, 659.25, 739.99, 987.77, 880.00, 830.61, 554.37, 659.25, 739.99, 987.77, 880.00, 830.61, 493.88, 622.25, 659.25, 739.99, 830.61, 0, 830.61, 739.99, 554.37 };
+char time[22] = { 12, 12, 8, 12, 12, 8, 12, 12, 8, 12, 12, 8, 12, 12, 8, 12, 12, 8, 1, 12, 12, 23 };
+unsigned char i = 0x00;
+unsigned char k = 0x00;
+unsigned char r = 0x00;
 
-
-
-
-
-double tone[13] = { 329.63,293.66,261.63,293.66,329.63,329.63,329.63,261.63,293.66,293.66,329.63,293.66,261.63 };
-unsigned char counter = 13;
-unsigned char onFlag = 0;
-enum music { Start, INIT, PLAY, WAIT } state;
-
-void button_Tick() {
-	switch (state) { // Transitions
-	case off:
-		if (!PINA * 0x01) {
-			state = play;
-			j = 0;
-		}
-		else
-			state = off;
-		break;
-	case play:
-		if (j < 13) {
-			state = play;
-		}
-		else {
-			state = off;
-		}
-
+void Tick()
+{
+	switch (state) //transitions
+	{
+	case Start:
+	{
+		state = WAIT;
 		break;
 	}
-	switch (state) { // State actions
-	case off:
-		set_PWM(0);
+	case WAIT:
+	{
+		if ((~PINA & 0x01) == 0x01)
+		{
+			state = PLAY; break;
+		}
+		else
+		{
+			state = WAIT; break;
+		}
+	}
+
+	case PLAY:
+	{
+		if (i <= time[k])
+		{
+			state = PLAY; break;
+		}
+		else if (i > time[k])
+		{
+			++r;
+			state = DOWN;
+			break;
+		}
+	}
+	case DOWN:
+	{
+		if (r < 24)
+		{
+			++k;
+			state = PLAY;
+			break;
+		}
+		else if (r >= 20)
+		{
+			state = PAUSE; break;
+		}
+	}
+
+	case PAUSE:
+	{
+		if ((~PINA & 0x01) == 0x01)
+		{
+			state = WAIT_2; break;
+		}
+		else
+		{
+			state = PAUSE; break;
+		}
+	}
+	case WAIT_2:
+	{
+		if ((~PINA & 0x01) == 0x01)
+		{
+			state = WAIT_2;
+			break;
+		}
+		else
+		{
+			state = WAIT;
+			break;
+		}
+	}
+	default:
 		break;
-	case play:
-		set_PWM(notes[i]);
-		set_PWM(0);
+	}
+	switch (state) //state actions
+	{
+	case Start:
+		break;
+
+	case WAIT:
+	{
+		i = 0;
+		k = 0;
+		r = 0;
+		break;
+	}
+
+	case PLAY:
+	{
+		set_PWM(notes[k]);
+		++i;
+		break;
+	}
+
+	case DOWN:
+	{
+		set_PWM(0); break;
+	}
+
+	case PAUSE:
+		break;
+	default:
 		break;
 	}
 }
+
 int main(void)
 {
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
-	state = Start;
 	PWM_on();
-	TimerSet(400);
+
+	TimerSet(62);
 	TimerOn();
+	state = Start;
 	while (1)
 	{
-
 		Tick();
 		while (!TimerFlag);
 		TimerFlag = 0;
-
 	}
 }
--- turnin/cyoh001_lab9_part3.c
+++ turnin/cyoh001_lab9_part3.c
@@ -1,9 +1,20 @@
+/*	Author:
+ *  	Partner(s) Name:
+ *   	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
 volatile unsigned char TimerFlag = 0;
 unsigned long _avr_timer_M = 1;
 unsigned long _avr_timer_cntcurr = 0;
+
 void TimerOn()
 {
 	TCCR1B = 0x0B;
@@ -36,35 +47,34 @@ void TimerSet(unsigned long M)
 	_avr_timer_cntcurr = _avr_timer_M;
 }
 
-void set_PWM(double frequency);
-void PWM_off();
-void PWM_on();
+void set_PWM(double frequency) {
+	static double current_frequency;
+	if (frequency != current_frequency) {
+		if (!frequency) TCCR3B &= 0x08;
+		else TCCR3B |= 0x03;
+		if (frequency < 0.954) OCR3A = 0xFFFF;
+		else if (frequency > 31250) OCR3A = 0x0000;
+		else OCR3A = (short)(8000000 / (128 * frequency)) - 1;
+		TCNT3 = 0;
+		current_frequency = frequency;
+	}
+}
+void PWM_on() {
+	TCCR3A = (1 << COM3A0);
+	TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+	set_PWM(0);
+}
+void PWM_off() {
+	TCCR3A = 0x00;
+	TCCR3B = 0x00;
+}
 
-
-void Tick();
 enum States { Start, WAIT, PLAY, DOWN, PAUSE, WAIT_2 }state;
 double notes[22] = { 830.61, 622.25, 659.25, 739.99, 987.77, 880.00, 830.61, 554.37, 659.25, 739.99, 987.77, 880.00, 830.61, 493.88, 622.25, 659.25, 739.99, 830.61, 0, 830.61, 739.99, 554.37};
 char time[22] = { 12, 12, 8, 12, 12, 8, 12, 12, 8, 12, 12, 8, 12, 12, 8, 12, 12, 8, 1, 12, 12, 23 };
 unsigned char i = 0x00;
 unsigned char k = 0x00;
 unsigned char r = 0x00;
-
-int main(void)
-{
-	DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0xFF; PORTB = 0x00;
-	PWM_on();
-
-	TimerSet(62);
-	TimerOn();
-	state = Start;
-	while (1)
-	{
-		Tick();
-		while (!TimerFlag);
-		TimerFlag = 0;
-	}
-}
 
 void Tick()
 {
@@ -173,42 +183,21 @@ void Tick()
 	}
 }
 
+int main(void)
+{
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	PWM_on();
 
-void set_PWM(double frequency) {
-
-
-	// Keeps track of the currently set frequency
-	// Will only update the registers when the frequency
-	// changes, plays music uninterrupted.
-	static double current_frequency;
-	if (frequency != current_frequency) {
-
-		if (!frequency) TCCR3B &= 0x08; //stops timer/counter
-		else TCCR3B |= 0x03; // resumes/continues timer/counter
-
-		// prevents OCR3A from overflowing, using prescaler 64
-		// 0.954 is smallest frequency that will not result in overflow
-		if (frequency < 0.954) OCR3A = 0xFFFF;
-
-		// prevents OCR3A from underflowing, using prescaler 64					// 31250 is largest frequency that will not result in underflow
-		else if (frequency > 31250) OCR3A = 0x0000;
-
-		// set OCR3A based on desired frequency
-		else OCR3A = (short)(8000000 / (128 * frequency)) - 1;
-
-		TCNT3 = 0; // resets counter
-		current_frequency = frequency;
+	TimerSet(62);
+	TimerOn();
+	state = Start;
+	while (1)
+	{
+		Tick();
+		while (!TimerFlag);
+		TimerFlag = 0;
 	}
 }
-void PWM_on() {
-	TCCR3A = (1 << COM3A0);
-	// COM3A0: Toggle PB6 on compare match between counter and OCR3A
-	TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
-	// WGM32: When counter (TCNT3) matches OCR3A, reset counter
-	// CS31 & CS30: Set a prescaler of 64
-	set_PWM(0);
-}
-void PWM_off() {
-	TCCR3A = 0x00;
-	TCCR3B = 0x00;
-}
+
+
