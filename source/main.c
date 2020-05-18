9ce1aef3ed90fe4c41dbb923c814dae2081cab22
Calvin Yoh <flashfire340@gmail.com>
2020-05-17 21:15:37 -0700

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,55 +1,22 @@
-/*	Author:
- *  	Partner(s) Name:
- *   	Lab Section:
+/*	Author: cyoh001
+ *  Partner(s) Name:
+ *	Lab Section:
  *	Assignment: Lab #  Exercise #
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
-
 #include <avr/io.h>
-#include <avr/interrupt.h>
 
-volatile unsigned char TimerFlag = 0;
-unsigned long _avr_timer_M = 1;
-unsigned long _avr_timer_cntcurr = 0;
+enum States { Start, ON, OFF, Wait } state;
+unsigned char tempA = 0x00;
 
-void TimerOn()
+void set_PWM(double frequency)
 {
-	TCCR1B = 0x0B;
-	OCR1A = 125;
-	TIMSK1 = 0x02;
-	TCNT1 = 0;
-	_avr_timer_cntcurr = _avr_timer_M;
-	SREG |= 0x80;
-}
-void TimerOff()
-{
-	TCCR1B = 0x00;
-}
-void TimerISR()
-{
-	TimerFlag = 1;
-}
-ISR(TIMER1_COMPA_vect)
-{
-	_avr_timer_cntcurr--;
-	if (_avr_timer_cntcurr == 0)
-	{
-		TimerISR();
-		_avr_timer_cntcurr = _avr_timer_M;
-	}
-}
-void TimerSet(unsigned long M)
-{
-	_avr_timer_M = M;
-	_avr_timer_cntcurr = _avr_timer_M;
-}
-
-void set_PWM(double frequency) {
 	static double current_frequency;
-	if (frequency != current_frequency) {
+	if (frequency != current_frequency)
+	{
 		if (!frequency) TCCR3B &= 0x08;
 		else TCCR3B |= 0x03;
 		if (frequency < 0.954) OCR3A = 0xFFFF;
@@ -69,115 +36,79 @@ void PWM_off() {
 	TCCR3B = 0x00;
 }
 
-enum States { Start, WAIT, PLAY, DOWN, PAUSE, WAIT_2 }state;
-double notes[22] = { 830.61, 622.25, 659.25, 739.99, 987.77, 880.00, 830.61, 554.37, 659.25, 739.99, 987.77, 880.00, 830.61, 493.88, 622.25, 659.25, 739.99, 830.61, 0, 830.61, 739.99, 554.37 };
-char time[22] = { 12, 12, 8, 12, 12, 8, 12, 12, 8, 12, 12, 8, 12, 12, 8, 12, 12, 8, 1, 12, 12, 23 };
-unsigned char i = 0x00;
-unsigned char k = 0x00;
-unsigned char r = 0x00;
-
 void Tick()
 {
-	switch (state) //transitions
+	tempA = ~PINA & 0x07;
+	switch (state) // transitions
 	{
 	case Start:
 	{
-		state = WAIT;
+		state = Wait;
 		break;
 	}
-	case WAIT:
+	case Wait:
 	{
-		if ((~PINA & 0x01) == 0x01)
+		if (tempA != 0x00)
 		{
-			state = PLAY; break;
-		}
-		else
-		{
-			state = WAIT; break;
-		}
-	}
-
-	case PLAY:
-	{
-		if (i <= time[k])
-		{
-			state = PLAY; break;
-		}
-		else if (i > time[k])
-		{
-			++r;
-			state = DOWN;
-			break;
-		}
-	}
-	case DOWN:
-	{
-		if (r < 24)
-		{
-			++k;
-			state = PLAY;
-			break;
-		}
-		else if (r >= 20)
-		{
-			state = PAUSE; break;
-		}
-	}
-
-	case PAUSE:
-	{
-		if ((~PINA & 0x01) == 0x01)
-		{
-			state = WAIT_2; break;
-		}
-		else
-		{
-			state = PAUSE; break;
-		}
-	}
-	case WAIT_2:
-	{
-		if ((~PINA & 0x01) == 0x01)
-		{
-			state = WAIT_2;
+			state = ON;
 			break;
 		}
 		else
 		{
-			state = WAIT;
+			state = Wait;
 			break;
 		}
+	}
+	case ON:
+	{
+		if (tempA != 0x00)
+		{
+			state = ON; break;
+		}
+		else
+		{
+			state = OFF; break;
+		}
+	}
+	case OFF:
+	{
+		state = Wait;
+		break;
 	}
 	default:
 		break;
 	}
-	switch (state) //state actions
+	switch (state)
 	{
-	case Start:
-		break;
-
-	case WAIT:
+	case Wait:
 	{
-		i = 0;
-		k = 0;
-		r = 0;
+		set_PWM(0);
 		break;
 	}
-
-	case PLAY:
+	case ON:
 	{
-		set_PWM(notes[k]);
-		++i;
+		if (tempA == 0x01)
+		{
+			set_PWM(261.63);
+			break;
+		}
+		else if (tempA == 0x02)
+		{
+			set_PWM(293.66);
+			break;
+		}
+		else if (tempA == 0x04)
+		{
+			set_PWM(329.63);
+			break;
+		}
 		break;
 	}
-
-	case DOWN:
+	case OFF:
 	{
-		set_PWM(0); break;
-	}
-
-	case PAUSE:
+		set_PWM(0);
 		break;
+	}
 	default:
 		break;
 	}
@@ -187,15 +118,11 @@ int main(void)
 {
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
+	state = Start;
 	PWM_on();
 
-	TimerSet(62);
-	TimerOn();
-	state = Start;
 	while (1)
 	{
 		Tick();
-		while (!TimerFlag);
-		TimerFlag = 0;
 	}
 }
